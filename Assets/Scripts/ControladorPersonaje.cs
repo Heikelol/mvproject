bbd36e66ee39c0611cdbeebd26877998ff1baea4
Julio Janeiro <furiorz@gmail.com>
2014-04-01 23:26:02 +0200

    Commit inicial

---
+++ Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorPersonaje : MonoBehaviour
+{
+    private float tiempo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float desplazamiento = Input.GetAxis("Horizontal");
+        if (desplazamiento > 0)
+        {
+            rigidbody2D.velocity = new Vector2(1, 0);
+            tiempo = 1;
+        } else if (desplazamiento < 0)
+        {
+            rigidbody2D.velocity = new Vector2(-1, 0);
+            tiempo = 1;
+        }
+        if (tiempo > 0)
+        {
+            tiempo -= Time.deltaTime;
+        } else
+        {
+            rigidbody2D.velocity = new Vector2(0, 0);
+        }
+    }
+}
